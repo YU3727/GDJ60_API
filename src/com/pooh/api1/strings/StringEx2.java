43c73468e46ad05d259e1faff5bcad9d7900b833
YU3727 <lacvert3727@naver.com>
2023-01-06 10:59:59 +0900

    230106-2 Ex2

--- src/com/pooh/api1/strings/StringEx2.java
+++ src/com/pooh/api1/strings/StringEx2.java
@@ -19,16 +19,17 @@ public class StringEx2 {
 		//2. 입력받은 파일명을 뒤에서부터 검색. 조건은 .의 위치 + .뒤에서부터 문자열 땡겨오기
 		//3. 있는거랑 비교, 이미지면 ok 아니면 정상적인 파일이 아니라고 출력
 		
+//		필요한 정보값이 나오는지 확인하기
 //		int n = fileName.lastIndexOf(".");
 //		System.out.println(n);		
-//		String modFile = fileName.substring(fileName.lastIndexOf(".")+1);
 //		System.out.println(modFile); // 확장자 추출은 됐음
 		
 		//파일명 배열의 데이터니까 for문 돌리고, if로 비교
 		//for문의 결과값은 check가 false거나 true거나
 		boolean check = false;
+		String modFile = fileName.substring(fileName.lastIndexOf(".")+1);
 		for(int i=0; i<names.length; i++) {
-			if(fileName.substring(fileName.lastIndexOf(".")+1).equals(names[i])) {
+			if(modFile.equals(names[i])) {
 				System.out.println("정상 파일입니다");
 				check = true;
 				break;
