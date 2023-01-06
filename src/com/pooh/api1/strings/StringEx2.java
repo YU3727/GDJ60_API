561b9c789fe940ad6e20fcee535f2cc65f9ae23d
YU3727 <lacvert3727@naver.com>
2023-01-06 10:42:49 +0900

    230106-ex2

--- src/com/pooh/api1/strings/StringEx2.java
+++ src/com/pooh/api1/strings/StringEx2.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class StringEx2 {
 //230106 1교시 2교시 Library(API) 에제
 	public static void main(String[] args) {
+		System.out.println("Start");
+		
 		Scanner sc = new Scanner(System.in);
 		String [] names = {"jpg", "jpeg", "gif", "png"};
 		
@@ -14,24 +16,29 @@ public class StringEx2 {
 		//이미지면 ok, 아니면 정상적인 파일이 아닙니다. 라고 출력하기
 		
 		//강사님 내용
-
-		String modFile = fileName.substring(fileName.lastIndexOf(".")+1);
-		System.out.println(modFile); // 확장자 추출은 됐음
+		//확장자 분리 작업
+		int idx = fileName.lastIndexOf(".");
+		String result = fileName.substring(idx+1);
+//		System.out.println(idx);
+//		System.out.println(result);
 		
-		//파일명 배열의 데이터니까 for문 돌리고, if로 비교
-		boolean check = false;
+		
+		//확장자 검증 작업 - for문이 끝났을 때는 두가지중 하나이다. 정상이 아니거나 정상이거나
+		boolean flag = false;
 		for(int i=0; i<names.length; i++) {
-			if(fileName.substring(fileName.lastIndexOf(".")+1).equals(names[i]) && !check) {
-				if(check) {
-					System.out.println("OK");
-					check = true;
-				}else if(check) {
-					System.out.println("정상적인 파일이 아닙니다");
-					break;
-				}
+			if(result.equals(names[i])) {
+				System.out.println("정상 파일입니다");
+				flag = true;
+				i = names.length;
+				break;
 			}
-
 		}
 		
+		//결과값 출력 - for문 안에 넣으면 돌때까지 계속 출력이라 따로 빼주자
+		if(!flag) {
+			System.out.println("비정상파일");
+		}
+		System.out.println("Finish");
 	}
+	
 }
