78d6056a1a057995d6e245aa171973ed2642e1aa
YU3727 <lacvert3727@naver.com>
2023-01-10 12:02:25 +0900

    230110-3
    강사님 해설

--- src/com/pooh/api2/collections/ex1/Ex1Main.java
+++ src/com/pooh/api2/collections/ex1/Ex1Main.java
@@ -4,7 +4,7 @@ public class Ex1Main {
 //230110 Library(API) java.util Package 1교시
 	public static void main(String[] args) {
 		
-		//학생은 이름 번호 국어 영어 수학 총점 평균으로 이루어짐
+		//학생은 이름 번호 국어 영어 수학 총점 평균으로 이루어짐 -> 학생은 이런 데이터로 이루어져 있다.
 		//뭐를 만들어야 하는가는 위 문장을 보고 유추해내야함(이거부터 잘 잡아야겠다)
 		//1.학생정보초기화
 		//2.학생정보전체조회
@@ -12,6 +12,9 @@ public class Ex1Main {
 		//4.학생정보추가
 		//5.학생정보삭제(이름으로 검색)
 		//6.프로그램종료
+		
+		StudentDAO dao = new StudentDAO();
+		dao.init();
 		
 		
 		
--- src/com/pooh/api2/collections/ex1/StudentDAO.java
+++ src/com/pooh/api2/collections/ex1/StudentDAO.java
@@ -1,5 +1,8 @@
 package com.pooh.api2.collections.ex1;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class StudentDAO {
 //230110 Library(API) java.util Package 1교시
 	//원래 여기서 쓰는 개념은 아니지만 한번 써보자
@@ -16,6 +19,42 @@ public class StudentDAO {
 		
 	}
 	
-	
+	//학생정보초기화
+	public ArrayList<StudentDTO> init() {
+		//학생의 데이터를 분리했을 때 모여야 학생이 되므로 Tokenizer를 쓰는게 낫다.
+		String data = this.sb.toString();
+		data = data.replace(" ", "-");
+		data = data.replace(",", "");
+		
+		System.out.println(data);
+		StringTokenizer st = new StringTokenizer(data, "-");
+		ArrayList<StudentDTO> ar = new ArrayList(); //ar은 호출되었을 때 값을 돌려주어야 하기 때문에 리턴타입이 되야함.
+
+		while(st.hasMoreTokens()) {
+			StudentDTO studentDTO = new StudentDTO(); //여러개의 데이터를 한 studentDTO 객체에 모음
+			studentDTO.setName(st.nextToken());
+			studentDTO.setNum(Integer.parseInt(st.nextToken()));
+			studentDTO.setKor(Integer.parseInt(st.nextToken()));
+			studentDTO.setEng(Integer.parseInt(st.nextToken()));
+			studentDTO.setMath(Integer.parseInt(st.nextToken()));
+			studentDTO.setTotal(studentDTO.getKor()+studentDTO.getEng()+studentDTO.getMath());
+			studentDTO.setAvg(studentDTO.getTotal()/3.0);
+			
+			ar.add(studentDTO);
+			//studentDTO 변수는 중괄호가 사라지면 없어지니까.... 이 정보를 저장해둘 무언가가 필요하다
+			//이전까진 배열이였지만, 이젠 ArrayList를 쓰면 된다.
+			
+//			//확인용
+//			System.out.println(st.nextToken());
+//			System.out.println(st.nextToken());
+//			System.out.println(st.nextToken());
+//			System.out.println(st.nextToken());
+//			System.out.println(st.nextToken());
+//			System.out.println("================");
+			
+		}
+			
+		return ar;
+	}
 	
 }
