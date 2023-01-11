04d8f3d3d3d0730d84d63a202cd61938b3e5709f
YU3727 <lacvert3727@naver.com>
2023-01-11 17:09:21 +0900

    230111-8

--- src/com/pooh/api2/collections/ex1/StudentController.java
+++ src/com/pooh/api2/collections/ex1/StudentController.java
@@ -33,7 +33,8 @@ public class StudentController {
 			//3.학생정보검색조회(이름으로 검색)
 			//4.학생정보추가
 			//5.학생정보삭제(이름으로 검색)
-			//6.프로그램종료
+			//6.학생정보백업(java.io.File을 이용)
+			//7.프로그램종료
 			System.out.println("작업을 선택해주세요");
 			System.out.println("1.학생정보초기화  2.학생전체조회  3.학생검색조회  4.학생정보추가  5.학생정보삭제  6.프로그램종료");
 			int select = sc.nextInt();
@@ -73,6 +74,8 @@ public class StudentController {
 				}
 				break;
 			case 6:
+				break;
+			case 7:
 				System.out.println("6.프로그램을 종료합니다");
 				check = false;
 			}
--- src/com/pooh/api2/collections/ex1/StudentDAO.java
+++ src/com/pooh/api2/collections/ex1/StudentDAO.java
@@ -23,6 +23,79 @@ public class StudentDAO {
 		
 	}
 	
+	
+	//학생정보삭제 - 학생들이 있어야 검색해서 지울 수 있다. 학생의 정보는 여기에 없으므로 매개변수를 쓰자
+	public int removeStudent (ArrayList<StudentDTO> ar) {
+		//삭제 확인용 변수 : 0이면 삭제 실패, 1이면 삭제 성공(내 임의대로 해도 됨)
+		int result = 0;
+		
+		System.out.println("삭제할 이름 입력");
+		String name = sc.next();
+		
+		//같은지 안같은지 비교해야하기 때문에 검색... - findByName 메서드와 유사 1. 2. 선택해서 하기
+		//1. index 번호를 쓰는경우 : ar.remove(index n)
+//		for(int i=0; i<ar.size(); i++) {
+//			if(name.equals(ar.get(i).getName())) {
+//				ar.remove(i); //찾은 i번쨰 index를 지우자
+//				result = 1;
+//				break;
+//			}
+//		}
+		
+		//2. object를 지우겠다 : ar.remove(object)
+		for(StudentDTO studentDTO:ar) {
+			if(name.equals(studentDTO.getName())) {
+				ar.remove(studentDTO); //꺼낸 DTO(주솔)를 지워주겠다
+				result = 1;
+				break;
+			}
+		}
+		return result; //0이면 못지움, 1이면 지웠다고 판단
+	}
+	
+	
+	//학생정보주기 - 어디서 정보를 받아와야한다. >> 매개변수로 정보를 받아와야한다.
+	public void addStudent(ArrayList<StudentDTO> ar) {
+		//학생 한명 만들기
+		StudentDTO studentDTO = new StudentDTO();
+		System.out.println("이름을 입력하세요");
+		studentDTO.setName(sc.next());
+		System.out.println("번호를 입력하세요");
+		studentDTO.setNum(Integer.parseInt(sc.next()));
+		System.out.println("국어 점수를 입력하세요");
+		studentDTO.setKor(sc.nextInt());
+		System.out.println("영어 점수를 입력하세요");
+		studentDTO.setEng(sc.nextInt());
+		System.out.println("수학 점수를 입력하세요");
+		studentDTO.setMath(sc.nextInt());
+		studentDTO.setTotal(studentDTO.getKor()+studentDTO.getEng()+studentDTO.getMath());
+		studentDTO.setAvg(studentDTO.getTotal()/3.0);
+		
+		ar.add(studentDTO); //arrayList에 만든 DTO를 넣어놓자
+		//return은 해줘도 되고 안해줘도 된다. - ArrayList는 새 객체를 만들지 않기 때문에 주소값이 변하지 않는다.
+		//주소값이 변하지 않은 상태에서 heap영역의 ar에 학생데이터가 하나 추가되기만 하기때문에 리턴하지 않아도 된다.
+	}
+	
+	
+	//학생정보검색 - 어디서 정보를 받아와야한다. >> 매개변수로 정보를 받아와야한다.
+	public StudentDTO findByName (ArrayList<StudentDTO> ar) {
+		System.out.println("검색할 이름을 입력하세요");
+		String name = sc.next();
+		
+		StudentDTO st = null; //찾으면 데이터 보내주고, 못찾으면 null을 보내주기 위함
+		
+		//이름찾기 반복 - ar은 size()를 통해 반복횟수 유추가 가능함
+		for(StudentDTO studentDTO : ar) {
+			if(name.equals(studentDTO.getName())) { //이름은 중복이 안된다는 가정하에. 같은 이름을 찾으면?
+				//이름을 찾는다는건 누군가가 필요하기때문에. > 보내줘야한다
+				st = studentDTO;
+				break; //향상된 for문은 조건식이 없기때문에 조건식을 false로 만드는 방법으로는 반복문을 종료할 수 없다.
+			}
+		}
+		return st; //st에는 학생 한명의 정보가 담긴 주소값이 들어있다.
+	}
+	
+	
 	//학생정보초기화
 	public ArrayList<StudentDTO> init() {
 		//학생의 데이터를 분리했을 때 모여야 학생이 되므로 Tokenizer를 쓰는게 낫다.
@@ -61,76 +134,6 @@ public class StudentDAO {
 	}
 	
 	
-	//학생정보검색 - 어디서 정보를 받아와야한다. >> 매개변수로 정보를 받아와야한다.
-	public StudentDTO findByName (ArrayList<StudentDTO> ar) {
-		System.out.println("검색할 이름을 입력하세요");
-		String name = sc.next();
-		
-		StudentDTO st = null; //찾으면 데이터 보내주고, 못찾으면 null을 보내주기 위함
-		
-		//이름찾기 반복 - ar은 size()를 통해 반복횟수 유추가 가능함
-		for(StudentDTO studentDTO : ar) {
-			if(name.equals(studentDTO.getName())) { //이름은 중복이 안된다는 가정하에. 같은 이름을 찾으면?
-				//이름을 찾는다는건 누군가가 필요하기때문에. > 보내줘야한다
-				st = studentDTO;
-				break; //향상된 for문은 조건식이 없기때문에 조건식을 false로 만드는 방법으로는 반복문을 종료할 수 없다.
-			}
-		}
-		return st; //st에는 학생 한명의 정보가 담긴 주소값이 들어있다.
-	}
-	
-	
-	//학생정보주기 - 어디서 정보를 받아와야한다. >> 매개변수로 정보를 받아와야한다.
-	public void addStudent(ArrayList<StudentDTO> ar) {
-		//학생 한명 만들기
-		StudentDTO studentDTO = new StudentDTO();
-		System.out.println("이름을 입력하세요");
-		studentDTO.setName(sc.next());
-		System.out.println("번호를 입력하세요");
-		studentDTO.setNum(Integer.parseInt(sc.next()));
-		System.out.println("국어 점수를 입력하세요");
-		studentDTO.setKor(sc.nextInt());
-		System.out.println("영어 점수를 입력하세요");
-		studentDTO.setEng(sc.nextInt());
-		System.out.println("수학 점수를 입력하세요");
-		studentDTO.setMath(sc.nextInt());
-		studentDTO.setTotal(studentDTO.getKor()+studentDTO.getEng()+studentDTO.getMath());
-		studentDTO.setAvg(studentDTO.getTotal()/3.0);
-		
-		ar.add(studentDTO); //arrayList에 만든 DTO를 넣어놓자
-		//return은 해줘도 되고 안해줘도 된다. - ArrayList는 새 객체를 만들지 않기 때문에 주소값이 변하지 않는다.
-		//주소값이 변하지 않은 상태에서 heap영역의 ar에 학생데이터가 하나 추가되기만 하기때문에 리턴하지 않아도 된다.
-	}
-	
-	
-	//학생정보삭제 - 학생들이 있어야 검색해서 지울 수 있다. 학생의 정보는 여기에 없으므로 매개변수를 쓰자
-	public int removeStudent (ArrayList<StudentDTO> ar) {
-		//삭제 확인용 변수 : 0이면 삭제 실패, 1이면 삭제 성공(내 임의대로 해도 됨)
-		int result = 0;
-		
-		System.out.println("삭제할 이름 입력");
-		String name = sc.next();
-		
-		//같은지 안같은지 비교해야하기 때문에 검색... - findByName 메서드와 유사 1. 2. 선택해서 하기
-		//1. index 번호를 쓰는경우 : ar.remove(index n)
-//		for(int i=0; i<ar.size(); i++) {
-//			if(name.equals(ar.get(i).getName())) {
-//				ar.remove(i); //찾은 i번쨰 index를 지우자
-//				result = 1;
-//				break;
-//			}
-//		}
-		
-		//2. object를 지우겠다 : ar.remove(object)
-		for(StudentDTO studentDTO:ar) {
-			if(name.equals(studentDTO.getName())) {
-				ar.remove(studentDTO); //꺼낸 DTO(주솔)를 지워주겠다
-				result = 1;
-				break;
-			}
-		}
-		return result; //0이면 못지움, 1이면 지웠다고 판단
-	}
 	
 	
 	
